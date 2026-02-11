7d5c620d4de2a2f4fc67a905c50ad83f20bf163d
Meet8376 <agrawalmeet11@gmail.com>
2026-02-11 13:39:04 +0530

    Refactor SJF scheduling algorithm implementation

--- sjf.c
+++ sjf.c
@@ -1,20 +1,44 @@
 #include<stdio.h>
-struct process
-{
-    int pid, at, bt,ct, tat, wt, rt;
-}p[3];
-int main(){
-    struct process p1;
-    int i,j;
-    for(i=0;i<3;i++) {
-        printf("Enter AT,BT for P%d \n",i+1);
-        scanf("%d%d",&p[i].at,&p[i].bt);
-        p[i].pid=i+1;
+int main() {
+    int n,i,j;
+    int bt[20],p[20],wt[20],tat[20];
+    float avg_wt=0,avg_tat=0;
+    int temp;
+    printf("Enter number of processes: ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++) {
+        p[i]=i+1;
+        printf("Enter Burst Time for P%d: ",i+1);
+        scanf("%d",&bt[i]);
     }
-    int n=3;
-    for(i=0;i<n-1;i++){
-        for(j=0;j<n-1-i;j++){
-            int temp= 
+    for(i=0;i<n;i++) {
+        for(j=i+1;j<n;j++) {
+            if(bt[i]>bt[j]) {
+                temp=bt[i];
+                bt[i]=bt[j];
+                bt[j]=temp;
+                temp=p[i];
+                p[i]=p[j];
+                p[j]=temp;
+            }
         }
     }
+    wt[0]=0;
+    for(i=1;i<n;i++) {
+        wt[i]=wt[i-1]+bt[i-1];
+    }
+    for(i=0;i<n;i++) {
+        tat[i]=wt[i]+bt[i];
+        avg_wt+=wt[i];
+        avg_tat+=tat[i];
+    }
+    avg_wt/=n;
+    avg_tat/=n;
+    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time");
+    for(i=0;i<n;i++) {
+        printf("\nP%d\t%d\t\t%d\t\t%d",p[i],bt[i],wt[i],tat[i]);
+    }
+    printf("\n\nAverage Waiting Time=%f",avg_wt);
+    printf("\nAverage Turnaround Time=%f",avg_tat);
+    return 0;
 }
